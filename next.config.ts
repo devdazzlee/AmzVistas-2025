2b4f14aecb3bde1a4d2962909039ca4e8ef7b60f
nabeelhassankhan19 <nabeelkhan@decryptedlabs.io>
2025-06-13 18:52:35 +0500

    Update Code AR

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- eslint: {
+  eslint: {
+    // don’t fail the build on ESLint errors
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! DANGER !!
+    // Allow production builds even if you have TS errors.
+    // Make sure you’re running `tsc --noEmit` separately if you still want type-checks.
+    ignoreBuildErrors: true,
   },
 };
 
